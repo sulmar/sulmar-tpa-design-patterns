ca0a3695af7c354ff157cd7574a9afa931d9ffb5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-02-14 09:43:22 +0100

    Add project files.

---
+++ BuilderPattern/Program.cs
@@ -0,0 +1,174 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Drawing.Layout;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Builder Pattern!");
+        }
+    }
+
+    public interface IPdfGeneratorService
+    {
+        PdfDocument CreatePdfReport(Guid tenantId);
+    }
+
+    public class Coordinate
+    {
+        public static readonly int x = 40;
+        public static readonly int y = 120 - 50;
+
+    }
+
+    public class LogoBuilder
+    {
+        public void BuildLogo(XGraphics gfx)
+        {
+            //var data = System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceNames();
+
+            var file = System.Reflection.Assembly.GetEntryAssembly()
+                .GetManifestResourceStream("CoreCitRateDetector.Resources.Logo.png");
+
+
+            int x = Coordinate.x + 200;
+            int y = Coordinate.y - 120 + 50;
+
+            gfx.DrawImage(XImage.FromStream(file), new XPoint(x, y));
+        }
+    }
+
+    public class ReportTitles
+    {
+        public ReportTitles()
+        {
+            Title = "Raport - możliwość zastosowania 9% stawki podatku CIT";
+            TitleEn = "Report - possibility to apply 9% CIT rate";
+            TenantNamePl = "Nazwa badanego podmiotu / Entity's name";
+            LagalFormPl = "Forma prawna / Legal form";
+            YearStartDatePl = "Pierwszy dzień badanego roku podatkowego / First day of a tested tax year";
+            YearEndDatePl = "Ostatni dzień badanego roku podatkowego / End of a tested tax year";
+            NetIncomePl = "Przychody (netto, bez podatku VAT) w badanym roku / Revenue (net of VAT) in the tested year";
+            GrossIncomePl = "Przychody ze sprzedaży (brutto, wraz z podatkiem VAT) w roku podatkowym poprzedzającym badany rok / Sales revenue (including VAT) in the tax year preceding the tested year";
+            CompanyEstablishmentDatePl = "Data rozpoczęcia działalności / Start date of business activity";
+            AlertPl = "UWAGA!Stawki 9 % nie stosuje się do przychodów / dochodów z zysków kapitałowych";
+            Footer = "© 2021 TPA Poland , ul. Młyńska 12, 61-730 Poznań. Wszelkie prawa zastrzeżone / All rights reserved.";
+            FooterEnglish = "All rigths reserved.";
+            //SubTitlePl = "Detekcja spółki nieruchomościowej";
+        }
+
+        public string Title { get; set; }
+        public string TitleEn { get; set; }
+        public string TenantNamePl { get; set; }
+        public string LagalFormPl { get; set; }
+        public string YearStartDatePl { get; set; }
+        public string YearEndDatePl { get; set; }
+        public string NetIncomePl { get; set; }
+        public string GrossIncomePl { get; set; }
+
+        public string CompanyEstablishmentDatePl { get; set; }
+        public string AlertPl { get; set; }
+        public string Asterisk { get; set; }
+        public string Footer { get; set; }
+        public string FooterEnglish { get; set; }
+        public string SubTitlePl { get; set; }
+    }
+    public class ReportHeader
+    {
+        public string ReportHeaderPl => "RAPORT";
+
+        public static void GetReportHeader(PdfPage page, XGraphics gfx)
+        {
+
+            int x = Coordinate.x;
+            int y = Coordinate.y + 60;
+            int width = 490;
+            int highth = 320;
+
+            XTextFormatter tf = new XTextFormatter(gfx);
+            tf.Alignment = XParagraphAlignment.Center;
+            ReportTitles recoHeader = new ReportTitles();
+            XRect rect = new XRect(x, y, width, highth);
+            XFont font = new XFont("Arial", 14, XFontStyle.Bold);
+            tf.DrawString(recoHeader.Title, font, XBrushes.Black, rect, XStringFormats.TopLeft);
+            rect = new XRect(x, y + 15, width, highth);
+            font = new XFont("Arial", 14, XFontStyle.BoldItalic);
+            tf.DrawString(recoHeader.TitleEn, font, XBrushes.Black, rect, XStringFormats.TopLeft);
+
+        }
+    }
+
+    public class PdfGeneratorService : IPdfGeneratorService
+    {
+        private readonly ITenantRepository tenantRepository;
+
+        public PdfDocument CreatePdfReport(Guid tenantId)
+        {
+            Tenant tenant = tenantRepository.GetById(tenantId);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.AddPage();
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            //Logo
+            LogoBuilder logo = new LogoBuilder();
+            logo.BuildLogo(gfx);
+
+            //Nagłówek
+            ReportHeader.GetReportHeader(page, gfx);
+
+            //Pytania
+            // Subheader.BuildSubheaderFinancialData(page, gfx, tenant);
+            // PdfReportResult.BuildSubheaderFinancialDataResult(page, gfx, tenant);
+            //Subheader.BuildSubheaderReducedTax(page, gfx);
+            //Subheader.BuildSubheaderNewCompany(page, gfx);
+            //Subheader.BuildSubheaderCompanySplit(page, gfx);
+
+            ////RECO RESULT
+
+            // Footer.BuildFooter(page, gfx);
+            return document;
+
+        }
+    }
+
+
+        public interface ITenantRepository
+    {
+        Tenant GetById(Guid id);
+    }
+
+    public class BaseModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+
+    public class LegalForm : BaseModel
+    {
+        public string Name { get; set; }
+        public string EnglishName { get; set; }
+        public IEnumerable<Tenant> Tenant { get; set; }
+    }
+
+    public class Tenant : BaseModel
+    {
+        public string Name { get; set; } //podaje użytkownik
+        public Guid LegalFormId { get; set; }//podaje użytkownik
+        public LegalForm LegalForm { get; set; }
+        public DateTime StartOfTaxYear { get; set; }//podaje użytkownik
+        public DateTime EndOfTaxYear { get; set; }//podaje użytkownik
+        public decimal LastYearGrossRevenue { get; set; }//podaje użytkownik
+        public decimal CurrentYearNetRevenue { get; set; }//podaje użytkownik
+        public DateTime YearOfBusinessStart { get; set; }//podaje użytkownik
+        public string EmailAdress { get; set; }
+
+    }
+}
---
+++ FactoryMethodPattern/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+
+namespace FactoryMethodPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Factory Method Pattern!");
+
+            VisitCalculateAmountTest();
+
+            // PaymentTest();
+        }
+
+        private static void VisitCalculateAmountTest()
+        {
+            while (true)
+            {
+                Console.Write("Podaj rodzaj wizyty: (N)FZ (P)rywatna (F)irma: ");
+                string visitType = Console.ReadLine();
+
+                Console.Write("Podaj czas wizyty w minutach: ");
+                if (double.TryParse(Console.ReadLine(), out double minutes))
+                {
+                    TimeSpan duration = TimeSpan.FromMinutes(minutes);
+
+                    Visit visit = new Visit(duration, 100);
+
+                    decimal totalAmount = visit.CalculateCost(visitType);
+
+                    if (totalAmount == 0)
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    else
+                       if (totalAmount >= 200)
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.WriteLine($"Total amount {totalAmount:C2}");
+
+                    Console.ResetColor();
+                }
+            }
+
+        }
+
+        private static void PaymentTest()
+        {
+            while (true)
+            {
+                Console.Write("Podaj kwotę: ");
+
+                decimal.TryParse(Console.ReadLine(), out decimal totalAmount);
+
+                Console.Write("Wybierz rodzaj płatności: (G)otówka (K)karta płatnicza (P)rzelew: ");
+
+                var paymentType = Enum.Parse<PaymentType>(Console.ReadLine());
+
+                Payment payment = new Payment(paymentType, totalAmount);
+
+                if (payment.PaymentType == PaymentType.Cash)
+                {
+                    CashPaymentView cashPaymentView = new CashPaymentView();
+                    cashPaymentView.Show(payment);
+                }
+                else
+                if (payment.PaymentType == PaymentType.CreditCard)
+                {
+                    CreditCardPaymentView creditCardView = new CreditCardPaymentView();
+                    creditCardView.Show(payment);
+                }
+                else
+                if (payment.PaymentType == PaymentType.BankTransfer)
+                {
+                    BankTransferPaymentView bankTransferPaymentView = new BankTransferPaymentView();
+                    bankTransferPaymentView.Show(payment);
+                }
+
+                string icon = GetIcon(payment);
+                Console.WriteLine(icon);
+            }
+
+
+
+        }
+
+        private static string GetIcon(Payment payment)
+        {
+            switch (payment.PaymentType)
+            {
+                case PaymentType.Cash: return "[100]";
+                case PaymentType.CreditCard: return "[abc]";
+                case PaymentType.BankTransfer: return "[-->]";
+
+                default: return string.Empty;
+            }
+        }
+    }
+
+    #region Visit
+
+    public class Visit
+    {
+        public DateTime VisitDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public decimal PricePerHour { get; set; }
+
+        private const decimal companyDiscountPercentage = 0.9m;
+
+        public Visit(TimeSpan duration, decimal pricePerHour)
+        {
+            VisitDate = DateTime.Now;
+            Duration = duration;
+            PricePerHour = pricePerHour;
+        }
+
+        public decimal CalculateCost(string kind)
+        {
+            decimal cost = 0;
+
+            if (kind == "N")
+            {
+                cost = 0;
+            }
+            else if (kind == "P")
+            {
+                cost = (decimal)Duration.TotalHours * PricePerHour;
+            }
+            else if (kind == "F")
+            {
+                cost = (decimal)Duration.TotalHours * PricePerHour * companyDiscountPercentage;
+            }
+
+            return cost;
+        }
+    }
+
+    #endregion
+
+    #region Payment
+
+    public class Payment
+    {
+        public DateTime PaymentDate { get; set; }
+        public PaymentType PaymentType { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public Payment(DateTime paymentDate, PaymentType paymentType, decimal totalAmount)
+        {
+            PaymentDate = paymentDate;
+            PaymentType = paymentType;
+            TotalAmount = totalAmount;
+        }
+
+        public Payment(PaymentType paymentType, decimal totalAmount)
+            : this(DateTime.Now, paymentType, totalAmount)
+        { }
+
+    }
+
+    public enum PaymentType
+    {
+        Cash,
+        CreditCard,
+        BankTransfer
+    }
+
+    public abstract class PaymentView
+    {
+        public abstract void Show(Payment payment);
+    }
+
+    public class CashPaymentView : PaymentView
+    {
+        public override void Show(Payment payment)
+        {
+            Console.WriteLine($"Do zapłaty {payment.TotalAmount}");
+            Console.Write("Otrzymano: ");
+            decimal.TryParse(Console.ReadLine(), out decimal receivedAmount);
+
+            decimal restAmount = payment.TotalAmount - receivedAmount;
+
+            if (restAmount > 0)
+            {
+                Console.WriteLine($"Reszta {restAmount}");
+            }
+        }
+    }
+
+    public class CreditCardPaymentView : PaymentView
+    {
+        public override void Show(Payment payment)
+        {
+            Console.WriteLine($"Do zapłaty {payment.TotalAmount}");
+
+            Console.WriteLine($"Nawiązywanie połączenia z bankiem...");
+
+            Console.WriteLine("Transakcja zautoryzowana");
+        }
+    }
+
+
+    public class BankTransferPaymentView : PaymentView
+    {
+        public override void Show(Payment payment)
+        {
+            Console.WriteLine($"Dane do przelewu {payment.TotalAmount}");
+        }
+    }
+
+    #endregion
+}
